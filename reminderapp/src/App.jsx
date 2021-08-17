e06b40af35a92c8917fcff833a23e16b570693dd
Naman Bhoj <namanbhoj99@gmail.com>
2021-08-17 14:06:05 +0530

    initial

---
+++ reminderapp/src/App.jsx
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+
+function App() {
+  return <Footer></Footer>;
+}
+
+export default App;
---
+++ reminderapp/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+// <!-- Footer -->
+
+function Footer() {
+  return (
+    <footer className="page-footer font-small special-color-dark pt-4">
+      <div className="container">
+        <div className="row"></div>
+
+        <div className="footer-copyright text-center py-3">
+          © 2021 Copyright:
+          <a href="https://namanbhoj.github.io">Naman Bhoj</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
